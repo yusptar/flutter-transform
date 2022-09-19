c92389e139d722c53b70584dc4b529508f6a99f5
yusptar <yusupvatar@gmail.com>
2022-09-19 09:25:38 +0700

    menambahkan transform widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -102,6 +103,73 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Transform.rotate(
+              angle: -45 * (pi / 180.0),
+              child: ElevatedButton(
+                child: const Text("Rotated button"),
+                onPressed: () {},
+              ),
+            ),
+            Transform(
+              transform: Matrix4.rotationZ(-45 * (pi / 180.0)),
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                child: const Text("Rotated button"),
+                onPressed: () {},
+              ),
+            ),
+            Transform.scale(
+              scale: 2.0,
+              child: ElevatedButton(
+                child: const Text("scaled up"),
+                onPressed: () {},
+              ),
+            ),
+            Transform(
+              transform: Matrix4.identity()..scale(2.0, 2.0),
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                child: const Text("scaled up (matrix)"),
+                onPressed: () {},
+              ),
+            ),
+            Transform.translate(
+              offset: const Offset(100, 300),
+              child: ElevatedButton(
+                child: const Text("translated to bottom"),
+                onPressed: () {},
+              ),
+            ),
+            Transform(
+              transform: Matrix4.translationValues(100, 300, 0),
+              child: ElevatedButton(
+                child: const Text("translated to bottom (matrix)"),
+                onPressed: () {},
+              ),
+            ),
+            Transform.translate(
+              offset: const Offset(70, 200),
+              child: Transform.rotate(
+                angle: -45 * (pi / 180.0),
+                child: Transform.scale(
+                  scale: 2.0,
+                  child: ElevatedButton(
+                    child: const Text("multiple transformations"),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+            Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.translationValues(70, 200, 0)
+                ..rotateZ(-45 * (pi / 180.0))
+                ..scale(2.0, 2.0),
+              child: ElevatedButton(
+                child: const Text("multiple transformations (matrix)"),
+                onPressed: () {},
+              ),
+            )
           ],
         ),
       ),
